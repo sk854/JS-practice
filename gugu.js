910f8cea2be1bfd12ec27d4dd83a43bd6ea7b4dd
sk854 <67946595+sk854@users.noreply.github.com>
2020-07-08 18:00:15 +0900

    Add files via upload

--- gugu.js
+++ gugu.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/gugu/:n1/:n2', function (req, res) {
+app.get('/gugu/:n1/:n2', function (req, res, next) {
   var i = req.params.n1;
   var j = req.params.n2;
-  s = i + "*" + j + "=" + (i * j);
-  res.end(s);
+  if (!isNaN(i) && !isNaN(j) && i != undefined && j != undefined) {
+    s = i + "*" + j + "=" + (i * j);
+    res.end(s);
+  } else { res.end('Error') }
 }).listen(3000);
